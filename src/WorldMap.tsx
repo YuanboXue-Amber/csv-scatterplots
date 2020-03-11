a1a7e433f267b8dc4eee92173b71ac1260ae473b
Yuanbo Xue <yuanboxue@outlook.com>
2020-03-11 18:04:34 +0100

    Add zoom and hover title to map

--- src/WorldMap.tsx
+++ src/WorldMap.tsx
@@ -15,8 +15,15 @@ export class WorldMap extends Component<{}, {}> {
     // const csvString =
     // `name,value\nLocke,4\nReyes,8\nFord,15\nJarrah,16\nShephard,23\nKwon,42`;
 
-    const worldTopo = await d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json');
+    const [worldTopo, worldSpec] = await Promise.all([
+      d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json'),
+      d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv') ]);
     const data = topojson.feature(worldTopo, worldTopo.objects.countries);
+
+    const countryNames = new Map();
+    worldSpec.forEach((country: any) => {
+      countryNames.set(country.iso_n3, country.name);
+    });
 
     const projection = d3.geoNaturalEarth1();
     const pathGenerator = d3.geoPath().projection(projection);
@@ -31,19 +38,33 @@ export class WorldMap extends Component<{}, {}> {
         .attr('height', height)
         .attr('width', width);
 
-    svg
+    const mapG = svg.append('g');
+
+    // zoom
+    const zoom = d3.zoom()
+      .scaleExtent([1, 8])
+      .on('zoom', () => {
+        mapG.attr('transform', d3.event.transform);
+      });
+    mapG.call(zoom as any);
+
+    // Add outline for map
+    mapG
       .selectAll('.sphere')
       .data([1])
       .join('path')
         .attr('class', 'sphere')
         .attr('d', d => pathGenerator({type: 'Sphere'}));
 
-    svg
+    // draw countries on map
+    mapG
       .selectAll('.country')
       .data(data.features)
       .join('path')
         .attr('class', 'country')
-        .attr('d', (d: any) => pathGenerator(d));
+        .attr('d', (d: any) => pathGenerator(d))
+        .append('title')
+          .text((d: any) => countryNames.get(d.id));
 
   }
 
